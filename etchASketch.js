b00056e96b12ac1caca7d1ab4b2f59c5826a87af
fola60 <afolabiadekanle@gmail.com>
2023-09-11 18:30:44 +0100

    etchASketch.js

--- etchASketch.js
+++ etchASketch.js
@@ -1,4 +1,32 @@
- const sketchbox = document.querySelector('#sketchBoxes');   
-    const boxes = document.createElement("div") ;
-    boxes.classList.add('box')
-    .appendChild(boxes);
+    const sketchBox = document.querySelector('#sketchBoxes');
+
+      for(i=0;i < 256;i++) { 
+      const boxes = document.createElement('div') ;
+      boxes.classList.add('box');
+      sketchBox.appendChild(boxes);
+    }
+
+    const boxPlural = document.querySelectorAll('.box');
+
+    function changeDiv(){
+ 
+         boxes.classList.add("changedColor")
+    }
+    
+    boxPlural.forEach(boxes => {
+      function changeDiv(){
+        boxes.classList.add("changedColor")
+    }
+      function removeDiv(){
+        boxes.classList.remove("changedColor")
+    }
+      boxes.addEventListener('mouseenter', changeDiv)
+      boxes.addEventListener('mousedown',removeDiv)
+    })
+     const button = document.querySelector('.btn')
+    //  function askUser(){
+    //     const pixelInput = prompt("How are you");
+    //   }
+    
+     button.addEventListener('click',askUser)
+     
