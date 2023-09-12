2bf96e575865abf7f37e076fda12bbcd9e893b98
fola60 <afolabiadekanle@gmail.com>
2023-09-12 17:31:51 +0100

    etchASketch.js

--- etchASketch.js
+++ etchASketch.js
@@ -1,18 +1,18 @@
     const sketchBox = document.querySelector('#sketchBoxes');
 
-      for(i=0;i < 256;i++) { 
+function createBox(){ 
+      for(i=0;i < boxInput ;i++) { 
       const boxes = document.createElement('div') ;
       boxes.classList.add('box');
       sketchBox.appendChild(boxes);
     }
+  }
+
 
     const boxPlural = document.querySelectorAll('.box');
-
     function changeDiv(){
- 
          boxes.classList.add("changedColor")
     }
-    
     boxPlural.forEach(boxes => {
       function changeDiv(){
         boxes.classList.add("changedColor")
@@ -23,10 +23,59 @@
       boxes.addEventListener('mouseenter', changeDiv)
       boxes.addEventListener('mousedown',removeDiv)
     })
-     const button = document.querySelector('.btn')
-    //  function askUser(){
-    //     const pixelInput = prompt("How are you");
-    //   }
-    
-     button.addEventListener('click',askUser)
+
+    function clearCanvas(){
+      boxPlural.forEach( boxes => {
+        boxes.classList.remove("changedColor")
+      })
+}
+
+    const button = document.querySelector('.btn')
+    function askUser(){
+        pixelInput = prompt("Enter squared pixel size amount for the box,Max(64).");
+        boxInput = pixelInput * pixelInput;
+        fontInput =  400 / pixelInput;
+         
+
+        createBox();
+        const boxPlural = document.querySelectorAll('.box');
+
+        boxPlural.forEach(boxes => {
+        boxes.style.width = `${fontInput}px`;
+        boxes.style.height = `${fontInput}px`;
+        })
+
+        boxPlural.forEach(boxes => {
+          function changeDiv(){
+            boxes.classList.add("changedColor")
+        }
+
+          function removeDiv(){
+            boxes.classList.remove("changedColor")
+        }
+
+          boxes.addEventListener('mouseenter', changeDiv)
+          boxes.addEventListener('mousedown',removeDiv)
+        })
+
+        function clearCanvas(){
+          boxPlural.forEach( boxes => {
+            boxes.classList.remove("changedColor")
+          })
+        }
+        clearBtn.addEventListener('click',clearCanvas);
+        
+      }
+          
+          button.addEventListener('click',askUser);
+          const clearBtn = document.querySelector('.clear');
+        
+
+
+          function clearCanvas(){
+            boxPlural.forEach( boxes => {
+              boxes.classList.remove("changedColor")
+            })
+      }
+    clearBtn.addEventListener('click',clearCanvas);
      
